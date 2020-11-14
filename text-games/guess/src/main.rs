2d10da3ed026f44fae9d78d9d19bc5a7ad8d5c3c
Syn-Nine <contact@syn-nine.com>
2020-11-14 14:29:05 -0500

    initial commit

---
+++ text-games/guess/src/main.rs
@@ -0,0 +1,46 @@
+// Guess the number game
+//
+// Based on the example in Rust language documentation:
+//   https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+//
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main()
+{
+  let secret = rand::thread_rng().gen_range(1, 101);
+  let mut tries = 6;
+
+  loop
+  {
+    println!("enter a guess, {} tries remaining", tries);
+  
+    let mut guess = String::new();
+
+    std::io::stdin().read_line(&mut guess).expect("failed to read line");
+
+    let guess: u32 = match guess.trim().parse()
+    {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+    
+    match guess.cmp(&secret)
+    {
+      Ordering::Less => println!("too small"),
+      Ordering::Greater => println!("too large"),
+      Ordering::Equal => 
+      {
+        println!("you win!");
+        break;
+      }
+    }
+
+    tries = tries - 1;
+    if tries == 0
+    {
+      println!("Game Over");
+      break;
+    }
+  }
+}
---
+++ text-games/rps/src/main.rs
@@ -0,0 +1,87 @@
+// Rock-Paper-Scissors Game
+//
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main()
+{
+  let mut p_score = 0;
+  let mut c_score = 0;
+  let mut tries = 5;
+  let max = tries;
+
+  println!("Let's play Rock-Paper-Scissors!");
+
+  let options = ["Rock".to_string(), "Paper".to_string(), "Scissors".to_string()];
+  let success = [2, 0, 1];
+
+  while tries > 0
+  {
+    
+    println!("Best out of {}, {} tries remaining. What is your guess? [r]ock, [p]aper, or [s]cissors?", max, tries);
+
+    let mut guess = String::new();
+    std::io::stdin().read_line(&mut guess).expect("failed to read line");
+
+    let guess = match guess.as_bytes()[0]
+    {
+      b'r' => 0,
+      b'p' => 1,
+      b's' => 2,
+      _ => continue
+    };
+
+    let comp = rand::thread_rng().gen_range(0, 3);  
+    println!("Computer: {}", options[comp]);
+    println!("Player: {}", options[guess]);
+
+    // 0 = Player
+    // 1 = Computer
+    // 2 = Tie
+    let mut win = 2; // tie by default
+    
+    if success[guess] == comp
+    {
+      win = 0;
+    }
+    else if comp != guess
+    {
+      win = 1;
+    }
+
+    tries = tries - 1;
+    match win
+    {
+      // Player Scores
+      0 =>
+      {
+        println!("Player Score!");
+        p_score = p_score + 1;
+      },
+
+      // Computer Scores
+      1 =>
+      {
+        println!("Computer Score!");
+        c_score = c_score + 1;
+      },
+
+      // Tie
+      _ =>
+      {
+        println!("Tie!");
+        tries = tries + 1;
+      }
+    }
+
+    println!("Score: P: {}, C: {}\n", p_score, c_score);
+  }
+
+  match p_score.cmp(&c_score)
+  {
+    Ordering::Greater =>  println!("PLAYER WINS GAME!"),
+    Ordering::Less =>     println!("COMPUTER WINS GAME!"),
+    Ordering::Equal =>    println!("GAME TIED!"),
+  }
+
+}
